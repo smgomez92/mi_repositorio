5d83c7c5cd34f5286e0666ae82b875b4deb95ab4
smgomez92 <santy.rosca@hotmail.com>
2019-02-07 09:26:37 -0500

    Archivos de la pratica de JS

---
+++ mijs.js
@@ -0,0 +1,14 @@
+function agrandar() {
+    document.getElementById('demo').style.fontSize = '35px';
+    document.getElementById('boton').style.display = 'none';
+    document.getElementById('boton1').style.display = 'block';
+    //window.alert('se agrandó');
+}
+function achicar() {
+    document.getElementById('demo').style.fontSize = '25px';
+    document.getElementById('boton1').style.display = 'none';
+    document.getElementById('boton').style.display = 'block';
+    //window.alert('se achicó');
+    
+}
+
